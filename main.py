d994427297895ff96a12dbc90012be123fb56a11
Taleb Nehme <taleb.nehme@ensta-bretagne.com>
2023-12-12 09:53:52 +0100

    hanoi

---
+++ main.py
@@ -0,0 +1,9 @@
+from hanoi import HanoiRG
+from bfs import bfs_search
+
+hanoi_rg = HanoiRG(nbStacks=3, nbDisks=3)
+start_state = hanoi_rg.root()
+
+result_states = bfs_search(hanoi_rg, start_state, hanoi_rg.is_final_state)
+hanoi_rg.print_results(result_states)
+
